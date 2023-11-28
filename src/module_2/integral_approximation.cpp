711f0d2896f57c19721e952fdbd97d74bfacde1d
Alexandr Elyovin <rusov200222@gmail.com>
2023-11-28 22:49:57 +0300

    (integral_approximation.cpp) lab done

--- src/module_2/integral_approximation.cpp
+++ src/module_2/integral_approximation.cpp
@@ -15,7 +15,8 @@ double ApproximateIntegral(double x_start, double x_end, int starting_n,
                            double (*func)(double),
                            double (*integration_method)(double, double,
                                                         int, double(double)),
-                           int k, double precision);
+                           int k, double &richardson_clarification,
+                           double precision);
 double target_func(double x);
 
 
@@ -24,29 +25,50 @@ int main() {
     double b = 2;
     int starting_n = 2;
     double precision = 1e-6;
+    
+    double rich_clarif_rectangle;
+    double rich_clarif_trapezoid;
+    double rich_clarif_simpson;
 
-    cout << std::setprecision(10) << endl;    
+    cout << "Function 2 * x + 1 / x" << endl << endl;
+    cout << "Real solution: " << endl;
+    cout << setprecision(13) << 3 + log(2) << endl << endl;
+ 
+    cout << "Mean Rectangle Integral Method" << endl;
     double rectangle_integral = ApproximateIntegral(a, b, starting_n,
                                                     &target_func,
                                                     &MeanRectanglesMethod,
-                                                    2, precision);
+                                                    2, rich_clarif_rectangle,
+                                                    precision);
+    cout << "Integral (mean rectangle method): ";
+    cout << setprecision(13) << rectangle_integral << endl;
+    cout << "Richardson clarification: ";
+    cout << setprecision(13) << rich_clarif_rectangle << endl << endl;
+    
 
+    cout << "Trapezoid Method" << endl;
     double trapezoid_integral = ApproximateIntegral(a, b, starting_n,
                                                     &target_func,
                                                     &TrapezoidalMethod,
-                                                    2, precision);
-
+                                                    2, rich_clarif_trapezoid,
+                                                    precision);
+    cout << "Integral (trapezoidal method): ";
+    cout << setprecision(13) << trapezoid_integral << endl;
+    cout << "Richardson clarification: ";
+    cout << setprecision(13) << rich_clarif_trapezoid << endl << endl;
+    
+    cout << "Simpson Method" << endl;
     double simpson_integral = ApproximateIntegral(a, b, starting_n,
                                                   &target_func,
                                                   &SimpsonMethod,
-                                                  4, precision);
+                                                  4, rich_clarif_simpson,
+                                                  precision);
 
+    cout << "Integral (simpson method): ";
+    cout << setprecision(13) << simpson_integral << endl;
+    cout << "Richardson clarification: ";
+    cout << setprecision(13) << rich_clarif_simpson << endl;
 
-
-    cout << "Function 2 * x + 1 / x" << endl;
-    cout << "Integral (mean rectangle method): " << rectangle_integral << endl;
-    cout << "Integral (trapezoidal method): " << trapezoid_integral << endl;
-    cout << "Integral (simpson method): " << simpson_integral << endl;
     return 0;
 }
 
@@ -59,6 +81,9 @@ double target_func(double x) {
 double MeanRectanglesMethod(double x_start, double x_end, int n,
                             double (*func)(double)) {
     double h = (x_end - x_start) / n;
+
+    cout << "Step: " << setprecision(5) << h << endl;
+
     double func_sum = 0;
     for (int i = 1; i <= n; ++i) {
         func_sum += func(x_start + (i - 0.5) * h);
@@ -70,6 +95,9 @@ double MeanRectanglesMethod(double x_start, double x_end, int n,
 double TrapezoidalMethod(double x_start, double x_end, int n,
                          double (*func)(double)) {
     double h = (x_end - x_start) / n;
+
+    cout << "Step: " << setprecision(5) << h << endl;
+
     double func_sum = 0.5 * (func(x_start) + func(x_end));
     for (int i = 1; i <= n - 1; ++i) {
         func_sum += func(x_start + i * h);
@@ -81,6 +109,9 @@ double TrapezoidalMethod(double x_start, double x_end, int n,
 double SimpsonMethod(double x_start, double x_end, int n,
                      double (*func)(double)) {
     double h = (x_end - x_start) / (2 * n);
+
+    cout << "Step: " << setprecision(5) << h << endl;
+
     double func_sum = func(x_start) + func(x_end);
     for (int i = 1; i <= 2 * n - 1; ++i) {
         int coef = (i % 2 + 1) * 2;
@@ -94,17 +125,22 @@ double ApproximateIntegral(double x_start, double x_end, int starting_n,
                            double (*func)(double),
                            double (*integration_method)(double, double,
                                                         int, double(double)),
-                           int k, double precision = 1e-6) {
+                           int k, double& richardson_clarification,
+                           double precision = 1e-6) {
     double curr_integral = integration_method(x_start, x_end,
                                               starting_n, func);
+    double prev_integral;
     double absolute_error; 
     int n = 2 * starting_n;
     do {
-        double prev_integral = curr_integral;
+        prev_integral = curr_integral;
         curr_integral = integration_method(x_start, x_end, n, func);
         n *= 2;
         absolute_error = abs(curr_integral - prev_integral);
     } while (absolute_error / (pow(2, k) - 1) >= precision);
+
+    richardson_clarification = curr_integral \
+        + (curr_integral - prev_integral) / (pow(2, k) - 1);
 
     return curr_integral;
 }
